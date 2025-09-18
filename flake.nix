6f87ef89431b32ac5d25a21514413f949968196c
bjvanbemmel <beauvanbemmel@live.nl>
2025-09-18 10:42:29 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "CS_Studenten with Nix flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            dotnetCorePackages.sdk_9_0
+          ];
+
+          name = "cs_studenten";
+        };
+      }
+    );
+}
